1d20749c37783f87daa784755ffb90c4aa12493d
saraalshater <gleave.alshater@gmail.com>
2022-06-15 02:02:37 +0300

    headerDone

---
+++ portfolio/src/components/HeaderHome.js
@@ -0,0 +1,33 @@
+import React from "react";
+import logo from "../assets/logo.png";
+import './HeaderHome.css';
+
+function HeaderHome(props) {
+  return (
+    <nav className="nav">
+      <div>
+        <a href="/" className="logo">
+          <img src={logo} />
+        </a>
+      </div>
+      <div>
+        <ul>
+          <li>
+            <a href="/uxui">UXUI</a>
+          </li>
+          <li>
+            <a href="/programming">PROGRAMMING</a>
+          </li>
+          <li>
+            <a href="/arts">ARTS</a>
+          </li>
+          <li>
+            <a href="/about-me">ABOUT ME</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+export default HeaderHome;
