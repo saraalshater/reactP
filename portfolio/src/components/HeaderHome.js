d5d1f2fa5799e21edeccf14f0a9ba20e7aa102d1
saraalshater <gleave.alshater@gmail.com>
2022-06-21 04:08:27 +0300

    nav fully responsive

--- portfolio/src/components/HeaderHome.js
+++ portfolio/src/components/HeaderHome.js
@@ -1,33 +1,45 @@
 import React from "react";
-import logo from "../assets/logo.png";
-import logoSmall from "../assets/logo.png"
-import './HeaderHome.css';
+import { Link } from "react-router-dom";
+// import logo from "../assets/logo.png";
+import logoSmall from "../assets/logo.png";
+import "./HeaderHome.css";
+import burgerMenu from "../assets/burger-menu.png"
+// import { FaBeer } from "react-icons/fa";
+import logoMobile from "../assets/logoMobile.png"
 
 function HeaderHome(props) {
   return (
     <nav className="nav">
-      <div>
-        <a href="/" className="logo">
-          <img src={logoSmall} />
-        </a>
-      </div>
-      <div>
-        <ul>
-          <li>
-            <a href="/uxui">UXUI</a>
-          </li>
-          <li>
-            <a href="/programming">PROGRAMMING</a>
-          </li>
-          <li>
-            <a href="/arts">ARTS</a>
-          </li>
-          <li>
-            <a href="/about-me">ABOUT ME</a>
-          </li>
-        </ul>
-      </div>
-    </nav>
+    <div>
+      <a href="/" className="logo">
+        <img src={logoSmall} />
+      </a>
+    </div>
+    <div>
+      <a href="/" className="logo-mobile">
+        <img src={logoMobile}/>
+      </a>
+    </div>
+    <div>
+      <ul>
+        <li>
+          <a href="/uxui">UXUI</a>
+        </li>
+        <li>
+          <a href="/programming">PROGRAMMING</a>
+        </li>
+        <li>
+          <a href="/arts">ARTS</a>
+        </li>
+        <li>
+          <a href="/about-me">ABOUT ME</a>
+        </li>
+      </ul>
+      <a href="/" className="burger-menu">
+      <img src={burgerMenu} />
+      </a>
+    </div>
+  </nav>
   );
 }
 
